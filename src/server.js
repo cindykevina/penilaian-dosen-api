0e68b4373e5eb53750ad72fe6f62d1c30066d00c
Cindy Kevina <113493253+cindykevina@users.noreply.github.com>
2025-06-20 20:40:46 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
